d83a2309459ea32def8b5ff812fffe8ed5039d51
Akash <akashgupta7484@gmail.com>
2024-11-29 11:39:12 +0530

    fifth

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,13 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { Toaster, toast } from 'react-hot-toast'
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')).render(
   
   // <StrictMode>
     <BrowserRouter>
+      <Analytics/>
       <App className='bg-gradient-to-r from-blue-950 to-black'/>
       <Toaster />
     </BrowserRouter>
