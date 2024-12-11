3b6a38caba782d2751bccae07e0e8119e330e3b3
Akash <akashgupta7484@gmail.com>
2024-12-11 19:05:35 +0530

    updated frontend port

--- Backend/server.js
+++ Backend/server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:5173', // Allow only your frontend
+  origin: `${import.meta.env.Frontend_URL}`, // Allow only your frontend
   methods: ['GET', 'POST'], // Specify allowed methods
   credentials: true // Allow credentials if needed
 }));
