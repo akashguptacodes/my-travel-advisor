ef9d21e5c4dbb00b791d5bcecb77d80995528c80
Akash <akashgupta7484@gmail.com>
2024-12-24 18:08:00 +0530

    priv route

--- frontend/src/components/custom/PrivateRoute.jsx
+++ frontend/src/components/custom/PrivateRoute.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import toast from 'react-hot-toast'
 import { Navigate } from 'react-router-dom'
 
 function PrivateRoute({loggedIn, children}) {
@@ -7,12 +6,7 @@ function PrivateRoute({loggedIn, children}) {
         return children
     }
     else{
-        setTimeout(() => {
-            toast.error("You must be logged in to view this page")
-        }, 1000)
         return <Navigate to = '/' />
-
     }
 }
-
 export default PrivateRoute
