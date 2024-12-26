4a8b4b49daff74796a1d0d98471d83cc5d17cbea
Akash <akashgupta7484@gmail.com>
2024-12-26 21:14:26 +0530

    cors

--- Backend/server.js
+++ Backend/server.js
@@ -5,12 +5,13 @@ const router = require('./routes/trip');
 require('dotenv').config();
 
 const app = express();
-app.use(cors({
-  origin: process.env.Frontend_URL,
-  methods: ['GET', 'POST'], // Specify allowed methods
-  credentials: true, // Allow credentials if needed
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors());
+// // {
+//   origin: process.env.Frontend_URL,
+//   methods: ['GET', 'POST'], // Specify allowed methods
+//   credentials: true, // Allow credentials if needed
+//   allowedHeaders: ['Content-Type', 'Authorization']
+// }
 console.log(process.env.Frontend_URL);
 
 app.use(express.json());
